1acb854fc5b41f1576f4630219e211b522d29782
여미지 <ydid02@naver.com>
2022-06-04 16:26:30 +0900

    Setting01

---
+++ app/src/main/java/com/study/android_singlebungle/DairyUploadActivity.java
@@ -0,0 +1,14 @@
+package com.study.android_singlebungle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DairyUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dairy_upload);
+    }
+}
---
+++ app/src/main/java/com/study/android_singlebungle/SettingProfileActivity.java
@@ -0,0 +1,14 @@
+package com.study.android_singlebungle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_profile);
+    }
+}
