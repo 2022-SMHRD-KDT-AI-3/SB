72c3e5bd94195634589e514096005605d29a1eac
이승윤 <margiella.lee@gmail.com>
2022-06-18 14:50:10 +0900

    중간확인용

--- Web/routes/router.js
+++ Web/routes/router.js
@@ -41,13 +41,17 @@ router.post("/login", function (request, response) {
 
   let sql = "select * from user where user_id = ?";
   conn.query(sql, [user_id], function (err, rows) {
+    console.log("test333");
 
     if (rows.length > 0) {
+
+      console.log("123123123123");
       request.session.user = {
         "user_id": rows[0].user_id,
         "doggy_name": rows[0].doggy_name,
         "diary_id": rows[0].diary_id
       }
+
       response.redirect("http://127.0.0.1:3000/mainDiary");
     } else {
       console.log("로그인 실패");
@@ -55,7 +59,90 @@ router.post("/login", function (request, response) {
   });
 });
 
-// 메인 페이지 라우터(ejs 변환)
+// 일기 작성 라우터(동영상 포함, 플라스크랑 주고 받아야 함) 1
+// router.post("/writeDiary", function (request, response) {
+//   let user_id = request.body.user_id;
+
+//   let sql = "select * from user where user_id = ?";
+//   conn.query(sql, [user_id], function (err, rows) {
+
+//     if (rows.length > 0) {
+//       request.session.user = {
+//         "user_id": rows[0].user_id,
+//         "doggy_name": rows[0].doggy_name,
+//         "diary_id": rows[0].diary_id
+//       }
+
+//       response.redirect("http://127.0.0.1:3000/mainDiary");
+//     } else {
+//       console.log("로그인 실패");
+//     }
+//   });
+// });
+
+// 일기 작성 라우터(동영상 포함, 플라스크랑 주고 받아야 함) 2
+// 단일 파일 업로드 라우터
+// 파일을 올리고 그 파일 경로를 플라스크로 주면 분석하고 결과값을 반환함
+router.post('/writeDiary', uploadWithOriginalFilename.single('chooseFile'), function (request, response) {
+
+  let diary_id = request.session.user.diary_id;
+  let diary_title = request.body.diary_title;
+  let diary_content = request.body.diary_content;
+  let video_path = request.file.path;
+
+  // 파일 업로드 부분 미리보기 기능 수정해야 함
+  console.log("0");
+
+  let sql = 'insert into diary(diary_id, title, content, date, video_path) values(?, ?, ?, now(), ?)'
+
+  ////
+  conn.query(sql, [diary_id, diary_title, diary_content, video_path], function(err, rows){
+    if(rows) {
+      console.log("1");
+      // response.redirect("");
+
+    } else {
+      console.log("2");
+    }
+
+
+
+  });
+  ////
+
+  console.log("3");
+  //alert('리뷰 작성 완료, 이전 페이지로 돌아갑니다');
+  // response.redirect("http://127.0.0.1:5000/action");
+});
+
+router.get("/refresh", function (request, response) {
+  console.log("옴?");
+  response.render("mainDiary.ejs",{
+  
+  });
+
+  console.log("여기는 옴???????옴?");
+
+
+  // response.redirect("http://127.0.0.1:3000/mainDiary");
+
+});
+
+// router.get('/action', uploadWithOriginalFilename.single('attachment'), function (request, response) {
+
+//   console.log(request.file);
+
+//   // response.redirect("http://127.0.0.1:3000/mainDiary");
+// });
+
+// router.get('/emotion', uploadWithOriginalFilename.single('attachment'), function (request, response) {
+
+//   console.log(request.file);
+
+//   // response.redirect("http://127.0.0.1:3000/mainDiary");
+// });
+
+// 메인 페이지 라우터(ejs 변환, 이따 고치기)
 router.get("/mainDiary", function (request, response) {
   let sql = "select * from diary;" + "select * from comment;";
   conn.query(sql, function(err, rows) {
@@ -68,7 +155,7 @@ router.get("/mainDiary", function (request, response) {
   });
 });
 
-// 마이 페이지 라우터(ejs 변환)
+// 마이 페이지 라우터(ejs 변환, 이따 고치기)
 router.get("/myDiary", function (request, response) {
   let sql = "select * from diary where diary_id = ?;" + "select * from comment;";
   conn.query(sql, [request.session.user.diary_id], function(err, rows) {
@@ -79,54 +166,6 @@ router.get("/myDiary", function (request, response) {
       comment : rows[1]
     }); // 마이 페이지가 랜더링될 때 유저 세션 정보, 내 일기 정보, 내 일기의 댓글 정보가 다 전달됨
   });
-});
-
-// 일기 작성 라우터(동영상 포함, 플라스크랑 주고 받아야 함) 1
-router.post("/writeDiary", function (request, response) {
-  let user_id = request.body.user_id;
-
-
-  let sql = "select * from user where user_id = ?";
-  conn.query(sql, [user_id], function (err, rows) {
-
-    if (rows.length > 0) {
-      request.session.user = {
-        "user_id": rows[0].user_id,
-        "doggy_name": rows[0].doggy_name,
-        "diary_id": rows[0].diary_id
-      }
-
-      response.redirect("http://127.0.0.1:3000/mainDiary");
-    } else {
-      console.log("로그인 실패");
-    }
-  });
-});
-
-// 일기 작성 라우터(동영상 포함, 플라스크랑 주고 받아야 함) 2
-// 단일 파일 업로드 라우터
-// 파일을 올리고 그 파일 경로를 플라스크로 주면 분석하고 결과값을 반환함
-router.post('/writeDiary', uploadWithOriginalFilename.single('attachment'), function (request, response) {
-
-  console.log(request.file);
-
-  response.redirect("http://127.0.0.1:5000/action");
-  response.redirect("http://127.0.0.1:5000/emotion");
-
-});
-
-router.get('/action', uploadWithOriginalFilename.single('attachment'), function (request, response) {
-
-  console.log(request.file);
-
-  // response.redirect("http://127.0.0.1:3000/mainDiary");
-});
-
-router.get('/emotion', uploadWithOriginalFilename.single('attachment'), function (request, response) {
-
-  console.log(request.file);
-
-  // response.redirect("http://127.0.0.1:3000/mainDiary");
 });
 
 // 댓글 작성 라우터
